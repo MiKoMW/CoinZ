b06f8fc353747237f57430d040e1b4ef8c4fabb8
Hu <shu2@ed.ac.uk>
2018-12-04 05:38:05 +0800

    User can set nick name!

--- app/src/main/java/io/github/mikomw/coinz/ui/activity/MapActivity.java
+++ app/src/main/java/io/github/mikomw/coinz/ui/activity/MapActivity.java
@@ -7,6 +7,7 @@ import android.location.Location;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.content.ContextCompat;
+import android.text.InputType;
 import android.util.Log;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -22,6 +23,7 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.mapbox.android.core.location.LocationEngine;
 import com.mapbox.android.core.location.LocationEngineListener;
@@ -42,6 +44,8 @@ import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
 import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode;
 import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode;
+import com.qmuiteam.qmui.widget.dialog.QMUIDialog;
+import com.qmuiteam.qmui.widget.dialog.QMUIDialogAction;
 
 import org.w3c.dom.Text;
 
@@ -124,8 +128,35 @@ public class MapActivity extends AppCompatActivity implements
 
         View headerView = navigationView.getHeaderView(0);
         userNickName = headerView.findViewById(R.id.drawer_NikeName);
+        userNickName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("USerNameClick");
+                final QMUIDialog.EditTextDialogBuilder builder =new QMUIDialog.EditTextDialogBuilder(MapActivity.this);
+                builder.setTitle("Set your nick name!").setPlaceholder("Please enter your nick name.")
+                        .setInputType(InputType.TYPE_CLASS_TEXT).addAction("Cancel", new QMUIDialogAction.ActionListener() {
+                    @Override
+                    public void onClick(QMUIDialog dialog, int index) {
+                        dialog.dismiss();
+                    }
+                }).addAction("Rename!", new QMUIDialogAction.ActionListener() {
 
+                    @Override            public void onClick(QMUIDialog dialog, int index) {
+                        CharSequence text =builder.getEditText().getText();
+                        if (text !=null && text.length() >0)
+                        {
+                            Toast.makeText(MapActivity.this, "Your nick name is: " + text, Toast.LENGTH_SHORT).show();
+                            MapActivity.this.user.setNickName(text.toString());
+                            userNickName.setText(user.getNickName());
+                            SerializableManager.saveSerializable(MapActivity.this,user,"userInfo.data");
+                            saveData();
+                            dialog.dismiss();
+                        }else {
+                            Toast.makeText(MapActivity.this, "Please enter your nick name.", Toast.LENGTH_SHORT).show();
+                        }}}).show();
 
+                }
+        });
 
     }
 
