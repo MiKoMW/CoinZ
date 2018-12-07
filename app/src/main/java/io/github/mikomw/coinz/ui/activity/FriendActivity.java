34916e83328c877e1b2fe08e203828a47c3f2867
Hu <shu2@ed.ac.uk>
2018-12-08 03:48:45 +0800

    Bug fixed for sale coins.

--- app/src/main/java/io/github/mikomw/coinz/ui/activity/FriendActivity.java
+++ app/src/main/java/io/github/mikomw/coinz/ui/activity/FriendActivity.java
@@ -6,7 +6,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.content.ContextCompat;
 import android.text.InputType;
